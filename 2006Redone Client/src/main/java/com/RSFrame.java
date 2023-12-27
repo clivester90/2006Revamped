9bcdea430b66ef3a958e1c2fd8fc6c3586580b78
Clive <clivester900@hotmail.com>
2023-12-27 22:24:56 +0000

    directory change
    change

--- 2006Redone Client/src/main/java/com/RSFrame.java
+++ 2006Redone Client/src/main/java/com/RSFrame.java
@@ -1,4 +1,4 @@
-package com.runescape;// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
+package com;// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
 // Jad home page: http://www.kpdus.com/jad.html
 // Decompiler options: packimports(3) 
 
